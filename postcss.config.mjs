9a1c1ce6d94fdb8a74f715cdae0c0ec297c1d08f
Noor Ilani <bla@Noors-MacBook-Air.local>
2026-01-06 12:07:36 +0100

    Update package dependencies, postcss config, globals.css, and remove tailwind.config.ts

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
-    "postcss-hover-media-feature": {},
+    "@tailwindcss/postcss": {},
+    // "postcss-hover-media-feature": {},
   },
 };
 
